60dc734159fba963818d796c1b43705caf63456e
yohannes57 <ytmderes@gmail.com>
2024-05-09 15:21:59 -0700

    first commit

---
+++ rLab.mjs
@@ -0,0 +1,5 @@
+//part one Refactoring old code
+// For the first part of this assignment, revisit your code from ALAB 308.3.1, wherein you create a script that parsed CSVs. Now that you have knowledge of arrays and objects, how would you change your approach to this problem? Take a few minutes to examine and refactor the code before continuing.
+
+//my ALAB308.3.1 code
+console.log("project started ")
